1ae46e6be70858fa9f2d418fa853c8635ce2b9a8
rachelmcclelland <rachelmcclelland97@hotmail.com>
2017-12-15 00:24:05 +0000

    Finished game

--- TicTacToe/MainPage.xaml.cs
+++ TicTacToe/MainPage.xaml.cs
@@ -28,6 +28,15 @@ namespace TicTacToe
         public MainPage()
         {
             this.InitializeComponent();
+            startGame();
+        }
+
+        private async void startGame()
+        {
+            var Msg = new MessageDialog("Press on square of your choice to begin \n\nPress Reset Game to clear scores ", "Instructions");
+            await Msg.ShowAsync();
+
+            displayTurn.Text = "Player 1";
         }
 
         //initialise turn, player 1 & plaler 2 score to 0
@@ -80,75 +89,57 @@ namespace TicTacToe
             checkColumns();
             checkDiagonal1();
             checkDiagonal2();
-
-            saveScore();
         }
 
         //check all rows to see if there is a winner
         private async void checkRows()
         {
 
+            //check top row
             if ((String)button1.Content != "" && (String)button2.Content != "" && (String)button3.Content != "")
             {
                 if ((button1.Content == button2.Content && button1.Content == button3.Content))
                 {
                     if ((String)button1.Content == "X")
                     {
-                        var Msg = new MessageDialog("Player 1 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player1++;
-                        score1.Text = player1.ToString();
+                        await player1Wins();
                     }
                     else
                     {
-                        var Msg = new MessageDialog("Player 2 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player2++;
-                        score2.Text = player2.ToString();
+                        await player2Wins();
                     }
-                    cleargame();
-                }  
+                }
             }
+
+            //check middle row
             if ((String)button4.Content != "" && (String)button5.Content != "" && (String)button6.Content != "")
             {
                 if ((button4.Content == button5.Content && button4.Content == button6.Content))
                 {
                     if ((String)button4.Content == "X")
                     {
-                        var Msg = new MessageDialog("Player 1 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player1++;
-                        score1.Text = player1.ToString();
+                        await player1Wins();
                     }
                     else
                     {
-                        var Msg = new MessageDialog("Player 2 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player2++;
-                        score2.Text = player2.ToString();
+                        await player2Wins();
                     }
-                    cleargame();
                 }
             }
+
+            //check bottom row
             if ((String)button7.Content != "" && (String)button8.Content != "" && (String)button9.Content != "")
             {
                 if ((button7.Content == button8.Content && button7.Content == button9.Content))
                 {
                     if ((String)button7.Content == "X")
                     {
-                        var Msg = new MessageDialog("Player 1 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player1++;
-                        score1.Text = player1.ToString();
+                        await player1Wins();
                     }
                     else
                     {
-                        var Msg = new MessageDialog("Player 2 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player2++;
-                        score2.Text = player2.ToString();
+                        await player2Wins();
                     }
-                    cleargame();
                 }
             }
         }
@@ -156,67 +147,51 @@ namespace TicTacToe
         //check all columns to see if there is a winner
         private async void checkColumns()
         {
+            //check left column
             if ((String)button1.Content != "" && (String)button4.Content != "" && (String)button7.Content != "")
             {
                 if ((button1.Content == button4.Content && button1.Content == button7.Content))
                 {
                     if ((String)button1.Content == "X")
                     {
-                        var Msg = new MessageDialog("Player 1 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player1++;
-                        score1.Text = player1.ToString();
+                        await player1Wins();
                     }
                     else
                     {
-                        var Msg = new MessageDialog("Player 2 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player2++;
-                        score2.Text = player2.ToString();
+                        await player2Wins();
                     }
-                    cleargame();
                 }
             }
+
+            //check middle column
             if ((String)button2.Content != "" && (String)button5.Content != "" && (String)button8.Content != "")
             {
                 if ((button2.Content == button5.Content && button2.Content == button8.Content))
                 {
                     if ((String)button4.Content == "X")
                     {
-                        var Msg = new MessageDialog("Player 1 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player1++;
-                        score1.Text = player1.ToString();
+                        await player1Wins();
                     }
                     else
                     {
-                        var Msg = new MessageDialog("Player 2 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player2++;
-                        score2.Text = player2.ToString();
+                        await player2Wins();
                     }
-                    cleargame();
                 }
             }
+
+            //check right column
             if ((String)button3.Content != "" && (String)button6.Content != "" && (String)button9.Content != "")
             {
                 if ((button3.Content == button6.Content && button3.Content == button9.Content))
                 {
                     if ((String)button7.Content == "X")
                     {
-                        var Msg = new MessageDialog("Player 1 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player1++;
-                        score1.Text = player1.ToString();
+                        await player1Wins();
                     }
                     else
                     {
-                        var Msg = new MessageDialog("Player 2 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player2++;
-                        score2.Text = player2.ToString();
+                        await player2Wins();
                     }
-                    cleargame();
                 }
             }
         }
@@ -230,19 +205,12 @@ namespace TicTacToe
                 {
                     if ((String)button1.Content == "X")
                     {
-                        var Msg = new MessageDialog("Player 1 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player1++;
-                        score1.Text = player1.ToString();
+                        await player1Wins();
                     }
                     else
                     {
-                        var Msg = new MessageDialog("Player 2 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player2++;
-                        score2.Text = player2.ToString();
+                        await player2Wins();
                     }
-                    cleargame();
                 }
             }
            
@@ -257,21 +225,34 @@ namespace TicTacToe
                 {
                     if ((String)button1.Content == "X")
                     {
-                        var Msg = new MessageDialog("Player 1 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player1++;
-                        score1.Text = player1.ToString();
+                        await player1Wins();
                     }
                     else
                     {
-                        var Msg = new MessageDialog("Player 2 Wins!", "Game Over");
-                        await Msg.ShowAsync();
-                        player2++;
-                        score2.Text = player2.ToString();
+                        await player2Wins();
                     }
-                    cleargame();
                 }
             }
+        }
+
+        //call method to displayer player one as winner as save their score
+        private async System.Threading.Tasks.Task player1Wins()
+        {
+            var Msg = new MessageDialog("Player 1 Wins!", "Game Over");
+            await Msg.ShowAsync();
+            player1++;
+            score1.Text = player1.ToString();
+            saveScore();
+        }
+
+        //call method to displayer player two as winner as save their score
+        private async System.Threading.Tasks.Task player2Wins()
+        {
+            var Msg = new MessageDialog("Player 2 Wins!", "Game Over");
+            await Msg.ShowAsync();
+            player2++;
+            score2.Text = player2.ToString();
+            saveScore();
         }
 
         //clear the grid
@@ -313,13 +294,15 @@ namespace TicTacToe
 
             try
             {
-                score1.Text = localSettings.Values["score1Setting"].ToString();
-
+                score1.Text = localSettings.Values["player1"].ToString();
+                score2.Text = localSettings.Values["player2"].ToString();
             }
             catch
             {
                 player1 = 0;
-                score1.Text = "";
+                score1.Text = "0";
+                player2 = 0;
+                score2.Text = "0";
             }
 
             base.OnNavigatedTo(e); //dont delete
@@ -329,13 +312,9 @@ namespace TicTacToe
         {
              ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
 
-            String score1Setting, score2Setting;
-
-            score1Setting = localSettings.Values["player1"].ToString();
-            score2Setting = localSettings.Values["player2"].ToString();
-
-
-
+            localSettings.Values["player1"] = player1.ToString();
+            localSettings.Values["player2"] = player2.ToString();
+            cleargame();
         }
 
 
